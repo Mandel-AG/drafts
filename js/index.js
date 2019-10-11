40581b0278adde697304424a43150eefeb944df6
Your Name <you@example.com>
2019-10-11 10:38:28 +0200

    step 2 100% et debut de la 3

--- js/index.js
+++ js/index.js
@@ -30,8 +30,7 @@ else if (e.target.getAttribute("data-form") != 1) {
     regist.style.display ="none";
 }
 }
-
-
+}
 
 
 
@@ -46,91 +45,91 @@ else if (e.target.getAttribute("data-form") != 1) {
 
 
 
-var prem = document.querySelector("#premier");
-var deux = document.querySelector("#deuxieme");
+// var prem = document.querySelector("#premier");
+// var deux = document.querySelector("#deuxieme");
 var square = document.querySelectorAll(".square-button");
 
 
 
+// var info = document.querySelectorAll(".form-control");
+
+// function recup(info){
+
+
+// for(v=0; v<info.length; v++){
+//  var valeur = info[v].value;
+
+
+// console.log(valeur);
+// }
+// }
+
+// prem.onclick = recup(info);
+
+// }
+
+
+
+
+
+var pass = document.getElementsByTagName("password");
+
+for (c = 0; c<pass.length; c++){
+    var valpass = pass[c].value;
+    var code = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+
+    if(valpass.match(code)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+
+
+premier.onclick = function(event){
+
+var square = document.querySelectorAll(".square-button")
 var info = document.querySelectorAll(".form-control");
 
-function recup(info){
+var prem = document.querySelector("#premier");
+var deux = document.querySelector("#deuxieme");
 
 
 for(v=0; v<info.length; v++){
- var valeur = info[v].value;
+
+var valeur = info[v].value;
 
 
 console.log(valeur);
 }
+
+
 }
 
-prem.onclick = recup(info);
 
+deuxieme.onclick = function(event){
+
+var square = document.querySelectorAll(".square-button")
+var info = document.querySelectorAll(".form-control");
+
+var prem = document.querySelector("#premier");
+var deux = document.querySelector("#deuxieme");
+
+
+for(v=0; v<info.length; v++){
+
+var valeur = info[v].value;
+
+
+console.log(valeur);
 }
 
-
-
-
-
-// var pass = document.getElementsByTagName("password");
-
-// for (c = 0; c<pass.length; c++){
-//     var valpass = pass[c].value;
-//     var code = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-
-//     if(valpass.match(code)){
-//         return true;
-//     }
-//     else{
-//         return false;
-//     }
-// }
-
-
-
-
-
-// premier.onclick = function(event){
-
-// var square = document.querySelectorAll(".square-button")
-// var info = document.querySelectorAll(".form-control");
-
-// var prem = document.querySelector("#premier");
-// var deux = document.querySelector("#deuxieme");
-
-
-// for(v=0; v<info.length; v++){
-
-// var valeur = info[v].value;
-
-
-// console.log(valeur);
-// }
-
-
-// }
-
-
-// deuxieme.onclick = function(event){
-
-// var square = document.querySelectorAll(".square-button")
-// var info = document.querySelectorAll(".form-control");
-
-// var prem = document.querySelector("#premier");
-// var deux = document.querySelector("#deuxieme");
-
-
-// for(v=0; v<info.length; v++){
-
-// var valeur = info[v].value;
-
-
-// console.log(valeur);
-// }
-
-// console.log(valeur);
-// }
+console.log(valeur);
+}
 
 
     // --------------------- STEP 3 -------------------------
@@ -151,6 +150,30 @@ prem.onclick = recup(info);
 
 // creer un autre fichier JS , et linker dnas le HTML avant l'autre link
 
+
+
+square.onclick = function(){
+
+
+class User {
+
+
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    getUsername(){return this.username}
+   
+
+}
+
+const utilisateur = new User (getUsername())
+
+alert(utilisateur);
+
+}
 
 
     // --------------------- STEP 4 -------------------------
