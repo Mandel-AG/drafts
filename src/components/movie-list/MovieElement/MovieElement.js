50e836026369963ff8de36d57818df431bc7b303
mandel <a.mandel06@gmail.com>
2020-05-29 02:20:10 +0200

    header

--- src/components/movie-list/MovieElement/MovieElement.js
+++ src/components/movie-list/MovieElement/MovieElement.js
@@ -13,7 +13,7 @@ mouseEnter = () =>{
             <div onMouseEnter={this.mouseEnter} className='w-50  p-2'>
                 <div className='border d-flex'>
                     <img width='150' height='200' src={this.props.movie.img} alt='film' />
-                    <div className='flex-fill d-flex flex-column p-3'>
+                    <div className='flex-fill d-flex flex-column p-3 '>
                         <h5>{this.props.movie.title}</h5>
                         <hr className='w-100'/>
                         <p>{this.props.movie.details}</p>
