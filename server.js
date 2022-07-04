9e306e3241e10948794df0aa558a0df53fb9f9ea
Mandel <a.mandel06@gmail.com>
2022-07-04 10:24:25 +0200

    test back

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 app.use(cors(corsOptions));
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 const db = require("./app/models");
@@ -28,12 +31,11 @@ db.mongoose
 
 
 
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
+require("./app/routes/tutorial.routes")(app);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
